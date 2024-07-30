c780767218a264a5ef4ca0fb378f94b391c5e3bc
Abkadar Chaudhari <abdulkadir4self@gmail.com>
2024-07-30 22:45:44 +0530

    partial home page done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Component/Header/Header';
+import Home from './Pages/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Header/Header.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import "./Header.css";
+
+function Header() {
+  const [active, setActive] = useState("");
+  const location = useLocation();
+
+  useEffect(() => {
+    setActive(location.pathname);
+  }, [location]);
+
+  return (
+    <nav className="navbar navbar-expand-lg container-fluid headerContainer sticky-top">
+      <div className="container  justify-content-center align-items-center">
+        <NavLink className="navbar-brand logo" to="/">
+        <p>Vya<span>par</span> Launch<span>pad</span></p>
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <span className="navbarBrandDiv">
+            <NavLink className="navbar-brand " to="/">
+            <p>Vya<span>par</span> Launch<span>pad</span></p>
+        </NavLink>
+        </span>
+          <ul className="navbar-nav">
+            
+            <li className="nav-item">
+              <NavLink
+                className={`nav-link ${active === "/" ? "active" : ""}`}
+                to="/"
+                aria-current="page"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className={`nav-link ${active === "/about" ? "active" : ""}`}
+                to="/about"
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className={`nav-link ${active === "/service" ? "active" : ""}`}
+                to="/service"
+              >
+                Services
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className={`nav-link ${active === "/blog" ? "active" : ""}`}
+                to="/blog"
+              >
+                Blog
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className={`nav-link ${active === "/contact" ? "active" : ""}`}
+                to="/contact"
+              >
+                Contact
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className={`nav-link ${active === "/sign-up" ? "active" : ""}`}
+                to="/sign-up"
+              >
+                Sign up
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default React.memo(Header);
